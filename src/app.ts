fac784024ac9e304fccae5f74d261961668fbe0c
Thom Poppins <thompoppins@gmail.com>
2023-09-24 11:16:27 +0100

    Error handling in Express

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,36 @@ const app = express();
 
 app.use(express.json());
 
-function middleware(req: Request, res: Response, next: NextFunction): any {
-  // @ts-ignore
-  req.name = "John Doe";
+const middleware =
+  ({ name }: { name: string }) =>
+  (req: Request, res: Response, next: NextFunction): any => {
+    res.locals.name = name;
 
-  next();
-}
+    next();
+  };
+
+app.use(middleware({ name: "John Doe" }));
 
 app.get(
   "/api/books/:bookId/:authorId",
-  middleware,
   (req: Request, res: Response, next: NextFunction) => {
-    // @ts-ignore
-    console.log(req.name);
-    // @ts-ignore
-    res.send(req.name);
+    console.log(res.locals.name);
+
+    res.send(res.locals.name);
   }
 );
+
+async function throwsError() {
+  throw new Error("Something went wrong");
+}
+
+app.get("/error", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await throwsError();
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(400).send("Something happened");
+  }
+});
 
 app.listen(5555, () => console.log("Application listening on port 5555"));
