df9c6d0f0bcc551bd616eb230d8d02639dd052f6
Thom Poppins <thompoppins@gmail.com>
2023-09-24 10:21:02 +0100

    Request parameters

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,10 @@ const app = express();
 
 app.use(express.json());
 
-app
-  .route("/")
-  .get((req: Request, res: Response) => {
-    return res.send("You made a GET request");
-  })
-  .post((req: Request, res: Response) => {
-    return res.send("You made a POST request");
-  })
-  .put((req: Request, res: Response) => {
-    return res.send("You made a PUT request");
-  })
-  .delete((req: Request, res: Response) => {
-    return res.send("You made a DELETE request");
-  })
-  .all((req: Request, res: Response) => {
-    return res.send("You made a request");
-  });
+app.get("/api/books/:bookId", (req: Request, res: Response) => {
+  console.log(req.params);
+  const bookId = req.params.bookId;
+  res.send(`Book with id ${bookId}`);
+});
 
 app.listen(5555, () => console.log("Application listening on port 5555"));
