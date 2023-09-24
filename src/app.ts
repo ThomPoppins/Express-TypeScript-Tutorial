d1ef672832b45ed59c6ba2786ef1a6b4fc0a3e7b
Thom Poppins <thompoppins@gmail.com>
2023-09-24 10:04:37 +0100

    Start Express.js server and listen to 3 routes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("Hello World");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({ success: true, name: "Thom Veldpaus", age: 32 });
+});
+
+app.post("/api/data", (req: Request, res: Response) => {
+  console.log(req.body);
+
+  return res.sendStatus(200);
+});
+
+app.all("/api/all", (req: Request, res: Response) => {
+  return res.sendStatus(200);
+});
+
+app.listen(5555, () => console.log("Application listening on port 5555"));
