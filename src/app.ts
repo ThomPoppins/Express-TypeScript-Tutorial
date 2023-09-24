8703c4133391ed08cc466bf253455fc13586aded
Thom Poppins <thompoppins@gmail.com>
2023-09-24 10:10:20 +0100

    Chain different methods to a route: GET POST PUT DELETE

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,19 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  return res.json({ success: true, name: "Thom Veldpaus", age: 32 });
-});
-
-app.post("/api/data", (req: Request, res: Response) => {
-  console.log(req.body);
-
-  return res.sendStatus(200);
-});
-
-app.all("/api/all", (req: Request, res: Response) => {
-  return res.sendStatus(200);
-});
+app
+  .route("/")
+  .get((req: Request, res: Response) => {
+    return res.send("You made a GET request");
+  })
+  .post((req: Request, res: Response) => {
+    return res.send("You made a POST request");
+  })
+  .put((req: Request, res: Response) => {
+    return res.send("You made a PUT request");
+  })
+  .delete((req: Request, res: Response) => {
+    return res.send("You made a DELETE request");
+  });
 
 app.listen(5555, () => console.log("Application listening on port 5555"));
