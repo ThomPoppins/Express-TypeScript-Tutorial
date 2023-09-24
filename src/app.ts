7bc90317c954324ac86574c0149dbdec2570111a
Thom Poppins <thompoppins@gmail.com>
2023-09-24 11:01:00 +0100

    Add middleware to route and use NextFunction

--- src/app.ts
+++ src/app.ts
@@ -4,26 +4,22 @@ const app = express();
 
 app.use(express.json());
 
-function handleGetBookOne(
-  req: Request,
-  res: Response,
-  next: NextFunction
-): any {
-  console.log(req.params);
+function middleware(req: Request, res: Response, next: NextFunction): any {
+  // @ts-ignore
+  req.name = "John Doe";
 
   next();
 }
 
-function handleGetBookTwo(
-  req: Request,
-  res: Response,
-  next: NextFunction
-): any {
-  console.log("second handler");
-
-  res.send(req.params);
-}
-
-app.get("/api/books/:bookId/:authorId", [handleGetBookOne, handleGetBookTwo]);
+app.get(
+  "/api/books/:bookId/:authorId",
+  middleware,
+  (req: Request, res: Response, next: NextFunction) => {
+    // @ts-ignore
+    console.log(req.name);
+    // @ts-ignore
+    res.send(req.name);
+  }
+);
 
 app.listen(5555, () => console.log("Application listening on port 5555"));
